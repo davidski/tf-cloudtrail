091cf6de2e93d62cc111b440a9877360ce416f59
David F. Severski <davidski@deadheaven.com>
2016-08-26 13:31:28 -0700

    Create CloudTrail audit logging

---
+++ cloudtrail.tf
@@ -0,0 +1,41 @@
+resource "aws_cloudtrail" "trail" {
+    name = "${var.trail_name}"
+    s3_bucket_name = "${aws_s3_bucket.bucket.id}"
+    s3_key_prefix = "cloudtrail"
+    include_global_service_events = true
+}
+
+resource "aws_s3_bucket" "bucket" {
+    bucket = "${var.audit_bucket}"
+    force_destroy = false
+    policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::${var.audit_bucket}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::${var.audit_bucket}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
---
+++ inputs.tf
@@ -0,0 +1,8 @@
+variable "trail_name" {
+    description = "Name of the CloudTrail trail"
+    default     = "cloudwatch-trail"
+}
+
+variable "audit_bucket" {
+    description = "S3 bucket name to store CloudTrail logs."
+}
