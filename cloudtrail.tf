3aa20266b36f099d877979f32c808ff7c7aea349
davidski <davidski@deadheaven.com>
2016-10-21 13:09:50 -0700

    Convert bucket_policy seperate resource. Ensure bucket name is externalizable.

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -1,41 +1,51 @@
 resource "aws_cloudtrail" "trail" {
     name = "${var.trail_name}"
-    s3_bucket_name = "${aws_s3_bucket.bucket.id}"
+    s3_bucket_name = "${var.audit_bucket}"
     s3_key_prefix = "cloudtrail"
     include_global_service_events = true
 }
 
-resource "aws_s3_bucket" "bucket" {
-    bucket = "${var.audit_bucket}"
-    force_destroy = false
-    policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AWSCloudTrailAclCheck",
-            "Effect": "Allow",
-            "Principal": {
-              "Service": "cloudtrail.amazonaws.com"
-            },
-            "Action": "s3:GetBucketAcl",
-            "Resource": "arn:aws:s3:::${var.audit_bucket}"
-        },
-        {
-            "Sid": "AWSCloudTrailWrite",
-            "Effect": "Allow",
-            "Principal": {
-              "Service": "cloudtrail.amazonaws.com"
-            },
-            "Action": "s3:PutObject",
-            "Resource": "arn:aws:s3:::${var.audit_bucket}/*",
-            "Condition": {
-                "StringEquals": {
-                    "s3:x-amz-acl": "bucket-owner-full-control"
-                }
-            }
-        }
-    ]
+resource "aws_s3_bucket_policy" "cloudtrail" {
+  bucket = "${var.audit_bucket}"
+  policy = "${data.aws_iam_policy_document.cloudtrail.json}"
 }
-POLICY
+
+data "aws_iam_policy_document" "cloudtrail" {
+  statement {
+    sid = "AWSCloudTrailAclCheck"
+    effect = "Allow"
+    principals = {
+      type = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    actions = [
+      "s3:GetBucketAcl"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.audit_bucket}"
+    ]
+  }
+  statement {
+    sid = "AWSCloudTrailWrite"
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.audit_bucket}/*"
+    ]
+    condition {
+      test = "StringEquals",
+      variable = "s3:x-amz-acl",
+      values = [ "bucket-owner-full-control" ]
+    }
+  }
+}
+
+output "cloudtrail" {
+    value = "${var.trail_name}"
 }
