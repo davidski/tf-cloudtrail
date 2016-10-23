b2c095cd9434139a4a0f0c49a5887f3de77b524d
davidski <davidski@deadheaven.com>
2016-10-22 20:17:09 -0700

    Move bucket policy out of Cloudtrail odule. Only 1 policy can be attached to a bucket at a time

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -1,51 +1,15 @@
 resource "aws_cloudtrail" "trail" {
-    name = "${var.trail_name}"
-    s3_bucket_name = "${var.audit_bucket}"
-    s3_key_prefix = "cloudtrail"
-    include_global_service_events = true
-}
+  name                          = "${var.trail_name}"
+  s3_bucket_name                = "${var.audit_bucket}"
+  s3_key_prefix                 = "cloudtrail"
+  include_global_service_events = true
 
-resource "aws_s3_bucket_policy" "cloudtrail" {
-  bucket = "${var.audit_bucket}"
-  policy = "${data.aws_iam_policy_document.cloudtrail.json}"
-}
-
-data "aws_iam_policy_document" "cloudtrail" {
-  statement {
-    sid = "AWSCloudTrailAclCheck"
-    effect = "Allow"
-    principals = {
-      type = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
-    actions = [
-      "s3:GetBucketAcl"
-    ]
-    resources = [
-      "arn:aws:s3:::${var.audit_bucket}"
-    ]
-  }
-  statement {
-    sid = "AWSCloudTrailWrite"
-    effect = "Allow"
-    principals {
-      type = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
-    actions = [
-      "s3:PutObject"
-    ]
-    resources = [
-      "arn:aws:s3:::${var.audit_bucket}/*"
-    ]
-    condition {
-      test = "StringEquals",
-      variable = "s3:x-amz-acl",
-      values = [ "bucket-owner-full-control" ]
-    }
+  tags = {
+    project    = "infrastructure"
+    managed_by = "Terraform"
   }
 }
 
 output "cloudtrail" {
-    value = "${var.trail_name}"
+  value = "${var.trail_name}"
 }
